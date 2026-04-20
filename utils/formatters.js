d1c94a697f0c6db8e29fa0391e6b7258975802c5
Ade <demiomotee@gmail.com>
2026-04-20 19:43:17 +0100

    Add utility functions for formatting currency and dates

---
+++ utils/formatters.js
@@ -0,0 +1,22 @@
+export function formatCurrency(amount) {
+  return new Intl.NumberFormat('en-GB', {
+    style: 'currency',
+    currency: 'GBP',
+    minimumFractionDigits: 2,
+  }).format(amount)
+}
+
+export function formatDate(dateString) {
+  if (!dateString) return ''
+  const date = new Date(dateString)
+  return new Intl.DateTimeFormat('en-GB', {
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric',
+  }).format(date)
+}
+
+export function toInputDate(dateString) {
+  if (!dateString) return ''
+  return dateString.split('T')[0]
+}
