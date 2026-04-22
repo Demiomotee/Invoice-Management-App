b31e70d88020b561b3ea373a1fb9d3132abc49e6
Ade <demiomotee@gmail.com>
2026-04-22 04:14:08 +0100

    Add padding to main content area in App component

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ export default function App() {
         <BrowserRouter>
           <div className="flex min-h-screen bg-[#F8F8FB] dark:bg-dark-1 font-sans transition-colors duration-200">
             <Sidebar />
-            <main className="flex-1 min-h-screen overflow-x-hidden">
+            <main className="flex-1 min-h-screen overflow-x-hidden pt-[72px] lg:pt-0">
               <Routes>
                 <Route path="/" element={<InvoiceList />} />
                 <Route path="/invoice/:id" element={<InvoiceDetail />} />
