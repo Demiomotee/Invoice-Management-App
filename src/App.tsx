e70f4a97113bd6ac705cb6d04e2c77b93f971560
Ade <demiomotee@gmail.com>
2026-04-23 04:01:37 +0100

    Wrap App component with ToastProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, useNavigate, useLocation } from 'react-ro
 import { useEffect } from 'react'
 import { ThemeProvider } from './context/ThemeContext'
 import { InvoiceProvider } from './context/InvoiceContext'
+import { ToastProvider } from './context/ToastContext'
 import Sidebar from './components/Sidebar'
 import InvoiceList from './pages/InvoiceList'
 import InvoiceDetail from './pages/InvoiceDetail'
@@ -9,20 +10,16 @@ import InvoiceDetail from './pages/InvoiceDetail'
 function BackButtonHandler() {
   const navigate = useNavigate()
   const location = useLocation()
-
   useEffect(() => {
     if (location.pathname !== '/') {
       window.history.pushState(null, '', window.location.href)
     }
     const handlePopState = () => {
-      if (location.pathname !== '/') {
-        navigate('/', { replace: true })
-      }
+      if (location.pathname !== '/') navigate('/', { replace: true })
     }
     window.addEventListener('popstate', handlePopState)
     return () => window.removeEventListener('popstate', handlePopState)
   }, [location.pathname, navigate])
-
   return null
 }
 
@@ -30,18 +27,22 @@ export default function App() {
   return (
     <ThemeProvider>
       <InvoiceProvider>
-        <BrowserRouter>
-          <BackButtonHandler />
-          <div className="flex min-h-screen bg-[#F8F8FB] dark:bg-dark-1 font-sans transition-colors duration-200">
-            <Sidebar />
-            <main className="flex-1 min-h-screen overflow-x-hidden pt-[72px] lg:pt-0">
-              <Routes>
-                <Route path="/" element={<InvoiceList />} />
-                <Route path="/invoice/:id" element={<InvoiceDetail />} />
-              </Routes>
-            </main>
-          </div>
-        </BrowserRouter>
+        <ToastProvider>
+          <BrowserRouter>
+            <BackButtonHandler />
+            <div className="flex min-h-screen bg-[#F8F8FB] dark:bg-dark-1 font-sans transition-colors duration-200">
+              <div className="relative z-[200]">
+                <Sidebar />
+              </div>
+              <main className="flex-1 min-h-screen overflow-x-hidden pt-[72px] lg:pt-0">
+                <Routes>
+                  <Route path="/" element={<InvoiceList />} />
+                  <Route path="/invoice/:id" element={<InvoiceDetail />} />
+                </Routes>
+              </main>
+            </div>
+          </BrowserRouter>
+        </ToastProvider>
       </InvoiceProvider>
     </ThemeProvider>
   )
